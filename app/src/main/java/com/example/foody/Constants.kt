6179a6955f8a6c50aabb63b2619936fb5f2a8f3b
pablorcruh <pablorcruh@gmail.com>
2023-01-06 21:20:50 -0500

    Avanzado hasta Create constant class

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.foody
+
+class Constants {
+    companion object{
+
+        const val BASE_URL="https://api.spoonacular.com"
+        const val API_KEY="2711e7acb9854e4ab6741f2aa574502f"
+
+    }
+}
