f5b200535171844958bf6b47d2c59ca566adaa31
pablorcruh <pablorcruh@gmail.com>
2023-01-07 20:43:33 -0500

    Avanzado hasta Create and Inject Repository class

---
+++ app/src/main/java/com/example/foody/data/RemoteDataSource.kt
@@ -0,0 +1,14 @@
+package com.example.foody.data
+
+import com.example.foody.data.network.FoodRecipeApi
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipeApi: FoodRecipeApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipeApi.getRecipes(queries)
+    }
+}
