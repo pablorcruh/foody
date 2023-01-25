93a8f786c88db844f96c97cb37f4a3d71d29f65d
pablorcruh <pablorcruh@gmail.com>
2023-01-24 22:35:07 -0500

    Avanzado hasta Send Result to DetailsActivity - Safe Args

--- app/src/main/java/com/example/foody/bindingAdapters/RecipesBinding.kt
+++ app/src/main/java/com/example/foody/bindingAdapters/RecipesBinding.kt
@@ -1,16 +1,33 @@
 package com.example.foody.bindingAdapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import com.example.foody.data.database.RecipeEntity
 import com.example.foody.models.FoodRecipe
 import com.example.foody.ui.NetworkResult
+import com.example.foody.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesBinding {
 
     companion object{
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: com.example.foody.models.Result){
+            recipeRowLayout.setOnClickListener {
+                try{
+                    val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                }catch(e: Exception){
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
         @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
         @JvmStatic
