20a76db01a51df9ef310402a80cbc33cff9b9d89
pablorcruh <pablorcruh@gmail.com>
2023-02-03 22:08:04 -0500

    Avanzado hasta Modify RecipeDao interface

--- app/src/main/java/com/example/foody/data/database/RecipesDao.kt
+++ app/src/main/java/com/example/foody/data/database/RecipesDao.kt
@@ -4,8 +4,10 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.foody.data.database.entities.FavoritesEntity
 import com.example.foody.data.database.entities.RecipeEntity
 import kotlinx.coroutines.flow.Flow
+import retrofit2.http.DELETE
 
 @Dao
 interface RecipesDao {
@@ -13,7 +15,18 @@ interface RecipesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipes(recipeEntity: RecipeEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoriteEntity: FavoritesEntity)
+
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipeEntity>>
 
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @DELETE
+    suspend fun deleteFavoriteRecipe(favoriteEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoritesRecipes()
 }
